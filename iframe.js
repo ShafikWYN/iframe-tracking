a4efa3fcba641d5eb6bf8403dd970579327b88c3
ShafikWYN <164982622+ShafikWYN@users.noreply.github.com>
2024-06-25 22:13:59 +0300

    Create iframe.js

---
+++ iframe.js
@@ -0,0 +1,34 @@
+function setupIframeFocusDetection() {
+    console.log('Iframe Focus Detection Setup');
+    window.addEventListener('blur', function() {
+        console.log('Window blurred');
+        setTimeout(function() {
+            var activeElement = document.activeElement;
+            console.log('Active Element:', activeElement);
+            if (activeElement && activeElement.tagName === 'IFRAME') {
+                console.log('Iframe detected');
+                pushIframeDetailsToDataLayer(activeElement);
+            }
+        }, 0);
+    });
+}
+
+function pushIframeDetailsToDataLayer(iframe) {
+    var iframeSrc = iframe.getAttribute('src');
+    var iframeId = iframe.getAttribute('id');
+    var iframeClass = iframe.getAttribute('class');
+    
+    console.log('Pushing to dataLayer:', {iframeSrc, iframeId, iframeClass});
+    
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'iframeFocus',
+        iFrame_src: iframeSrc || '',
+        iFrame_id: iframeId || '',
+        iFrame_class: iframeClass || '',
+        baseURL: window.location.href,
+        Keys: '' 
+    });
+}
+
+setupIframeFocusDetection();
